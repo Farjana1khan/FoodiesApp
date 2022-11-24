a74390ff9830703bb1f3999a9217a3a76afa7283
Farjana Khan <farjana09khan@gmail.com>
2022-11-24 10:00:49 +0530

    add foodies app components

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return (
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">FoodiesApp.com</p>
+                <p>FoodiesApp is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
+                <p>&copy; 2022 | All Rights Reserved</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>foodiesapp@gmail.com</p>
+                <p>+342-5324-9454</p>
+                <p>2393 Street Gurgaon </p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Github</p>
+                <p>LinkedIn</p>
+                <p>Telegram</p>
+            </div>
+        </div>
+    )
+}
